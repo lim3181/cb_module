678f2c0eaf701287593f5bf64b2e35dc42680188
JunSeong <JunSeong@DESKTOP-V14KV2S>
2019-06-17 15:30:58 +0900

    3 test

--- src/main/java/com/poc/spring/controller/MainController.java
+++ src/main/java/com/poc/spring/controller/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
-	@RequestMapping("/sds")
+	@RequestMapping("/")
 	public String getMethod() { 
 		return "index"; 
 	} 
